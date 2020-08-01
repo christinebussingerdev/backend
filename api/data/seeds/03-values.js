e334545717956514c95f2569012eec628403c589
Christine Bussinger <christinebussingerdev@gmail.com>
2020-07-31 20:05:34 -0400

    changed seeds to del and not trunc

--- api/data/seeds/03-values.js
+++ api/data/seeds/03-values.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('values').truncate()
+  return knex('values').del()
     .then(function () {
       // Inserts seed entries
       return knex('values').insert([
